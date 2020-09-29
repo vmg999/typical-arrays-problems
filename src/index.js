b970c66e1ec877dd9a84ce0ef13b99a92b2d68c4
vmg999 <vmg999@gmail.com>
2020-09-29 14:05:40 +0300

    feat: implement min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+	if(Array.isArray(array) && array.length != 0){
+		console.log(array);
+		return Math.min(...array);
+	}else{return 0;}
 }
 
 exports.max = function max (array) {
-  return 0;
+	if(Array.isArray(array) && array.length != 0){
+		return Math.max(...array);
+	}else{return 0;}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if(Array.isArray(array) && array.length != 0){
+		let sum=0;
+		for(let i=0;i<array.length;i++){
+			sum+=array[i];
+		}
+		return sum/array.length;
+	}else{return 0;}
 }
